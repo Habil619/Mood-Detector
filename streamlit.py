8c0f841df1a0fe8233d0d50d649278e720a51e4a
Habil619 <90960320+Habil619@users.noreply.github.com>
2023-10-28 18:06:52 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,54 +1,65 @@
 import streamlit as st
-import cv2
-import numpy as np
-from tensorflow import keras
-from PIL import Image
+import speech_recognition as sr
+from textblob import TextBlob
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
-# Load your trained model for face detection (you might need a separate model or a pre-trained model for this)
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# Initialize variables to store mood data
+mood_data = {'Happy': 0, 'Sad': 0, 'Neutral': 0}
+weekly_mood = []
 
-# Load your trained model for emotion recognition
-model = keras.models.load_model("your_emotion_model.h5")
+# Function to update mood data and weekly mood
+def update_mood(text):
+    global mood_data
+    global weekly_mood
 
-st.title("Real-time Face Emotion Detection")
+    analysis = TextBlob(text)
+    sentiment = analysis.sentiment.polarity
 
-# Initialize the webcam
-cap = cv2.VideoCapture(0)
+    if sentiment > 0:
+        mood = 'Happy'
+    elif sentiment < 0:
+        mood = 'Sad'
+    else:
+        mood = 'Neutral'
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+    mood_data[mood] += 1
+    weekly_mood.append(mood)
 
-    # Perform face detection
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+# Function to analyze weekly overall mood
+def get_weekly_mood():
+    week_start = datetime.now() - timedelta(days=datetime.now().weekday())
+    week_mood = [mood_data[m] for m in weekly_mood if (datetime.now() - timedelta(days=datetime.now().weekday())) <= week_start]
+    weekly_mood.clear()
 
-    for (x, y, w, h) in faces:
-        # Preprocess the detected face
-        face = gray[y:y + h, x:x + w]
-        face = cv2.resize(face, (48, 48))
-        face = np.expand_dims(face, axis=0)
-        face = face / 255.0
+    if not week_mood:
+        return "No data"
+    
+    avg_mood = sum(week_mood) / len(week_mood)
+    
+    if avg_mood > 0:
+        return 'Happy'
+    elif avg_mood < 0:
+        return 'Sad'
+    else:
+        return 'Neutral'
 
-        # Use your trained model to predict the emotion
-        emotion_labels = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
-        emotion_probabilities = model.predict(face)
-        emotion_index = np.argmax(emotion_probabilities)
-        emotion_label = emotion_labels[emotion_index]
+# Streamlit app
+st.title("Mood Analyzer")
 
-        # Draw a rectangle around the detected face
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+# Record and analyze speech
+if st.button("Record and Analyze"):
+    user_input = speech_to_text()
+    update_mood(user_input)
+    st.write(f"You said: {user_input}")
+    st.write(f"Mood: {get_weekly_mood()}")
 
-        # Display the emotion label
-        cv2.putText(frame, emotion_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+# Display mood statistics as a pie chart
+st.write("Mood Statistics:")
+fig, ax = plt.subplots()
+ax.pie(mood_data.values(), labels=mood_data.keys(), autopct='%1.1f%%', startangle=140)
+ax.axis('equal')
+st.pyplot(fig)
 
-    # Convert the OpenCV frame to a format Streamlit can display
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    # Display the frame with the emotion labels
-    st.image(frame, use_column_width=True)
-    st.write("Emotion: " + emotion_label)
-
-# Close the webcam
-cap.release()
+# Display weekly overall mood
+st.write(f"Weekly Overall Mood: {get_weekly_mood()}")
