446760dcb53449ae43f10430bd4db163a12fd4cc
Habil619 <90960320+Habil619@users.noreply.github.com>
2023-10-28 18:00:41 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,8 +4,11 @@ import numpy as np
 from tensorflow import keras
 from PIL import Image
 
-# Load your trained model
-model = keras.models.load_model("your_model.h5")
+# Load your trained model for face detection (you might need a separate model or a pre-trained model for this)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Load your trained model for emotion recognition
+model = keras.models.load_model("your_emotion_model.h5")
 
 st.title("Real-time Face Emotion Detection")
 
@@ -17,18 +20,35 @@ while True:
     if not ret:
         break
 
-    # Perform face detection (you can use OpenCV or other libraries)
-    # ...
+    # Perform face detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Preprocess the detected face (resize, normalize, etc.)
-    # ...
+    for (x, y, w, h) in faces:
+        # Preprocess the detected face
+        face = gray[y:y + h, x:x + w]
+        face = cv2.resize(face, (48, 48))
+        face = np.expand_dims(face, axis=0)
+        face = face / 255.0
 
-    # Use your trained model to predict the emotion
-    # ...
+        # Use your trained model to predict the emotion
+        emotion_labels = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
+        emotion_probabilities = model.predict(face)
+        emotion_index = np.argmax(emotion_probabilities)
+        emotion_label = emotion_labels[emotion_index]
 
-    # Display the frame with the emotion label
+        # Draw a rectangle around the detected face
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Display the emotion label
+        cv2.putText(frame, emotion_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+    # Convert the OpenCV frame to a format Streamlit can display
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Display the frame with the emotion labels
     st.image(frame, use_column_width=True)
-    st.write("Emotion: Emotion_Label")
+    st.write("Emotion: " + emotion_label)
 
 # Close the webcam
 cap.release()
